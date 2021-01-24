79f897d00c93c07717c4e018bcced2f1fbfd37d7
nacro90 <orcan.tiryakioglu@gmail.com>
2021-01-24 18:39:47 +0300

    Add new functions

--- cofun.lua
+++ cofun.lua
@@ -23,6 +23,21 @@ function cofun.take(n, iterator)
    end)
 end
 
+function cofun.skip(n, iterator)
+   return coroutine.wrap(function()
+      local counter = 1
+      local iteration = iterator()
+      while iterator and counter <= n do
+         iteration = iterator()
+         counter = counter + 1
+      end
+      while iterator do
+         coroutine.yield(iteration)
+         iteration = iterator()
+      end
+   end)
+end
+
 function cofun.map(mapper, iterator)
    return coroutine.wrap(function()
       for iteration in iterator do coroutine.yield(mapper(iteration)) end
@@ -85,18 +100,96 @@ function cofun.recur(iteration)
    end)
 end
 
-function cofun.enumerate(iterator) return cofun.zip(cofun.count(), iterator) end
-
-function cofun.iter(tbl)
+function cofun.recurwith(iteration_fun)
    return coroutine.wrap(function()
-      for _, iteration in pairs(tbl) do coroutine.yield(iteration) end
+      while true do coroutine.yield(iteration_fun()) end
    end)
 end
+
+function cofun.enumerate(iterator) return cofun.zip(cofun.count(), iterator) end
+
+function cofun.iter(iterable)
+   return coroutine.wrap(function()
+      if type(iterable) == 'table' then
+         for _, iteration in pairs(iterable) do
+            coroutine.yield(iteration)
+         end
+      elseif type(iterable) == 'string' then
+         for i = 1, #iterable do coroutine.yield(iterable:sub(i, i)) end
+      else
+         error('Expected string or table')
+      end
+   end)
+end
+
+function cofun.consume(iterator) for _ in iterator do end end
+
+function cofun.chain(...)
+   local iterators = {...}
+   return coroutine.wrap(function()
+      for _, iterator in ipairs(iterators) do
+         for iteration in iterator do coroutine.yield(iteration) end
+      end
+   end)
+end
+
+function cofun.prepend(...)
+   local args = {...}
+   local iterator = args[#args]
+   args[#args] = nil
+   return cofun.chain(cofun.iter(args), iterator)
+end
+
+function cofun.append(...)
+   local args = {...}
+   local iterator = args[#args]
+   args[#args] = nil
+   return cofun.chain(iterator, cofun.iter(args))
+end
+
+function cofun.cycle(iterator)
+   return coroutine.wrap(function()
+      local collection = {}
+      for iteration in iterator do
+         coroutine.yield(iteration)
+         collection[#collection + 1] = iteration
+      end
+      if #collection > 0 then
+         while true do
+            for _, v in ipairs(collection) do coroutine.yield(v) end
+         end
+      end
+   end)
+end
+
+function cofun.nwise(n, iterator)
+   return coroutine.wrap(function()
+      local iterations = {}
+      local iteration
+      for i = 1, n do
+         iteration = iterator()
+         if not iteration then return end
+         iterations[i] = iteration
+      end
+      repeat
+         coroutine.yield(unpack(iterations))
+         iteration = iterator()
+         for i = 1, n - 1 do iterations[i] = iterations[i + 1] end
+         iterations[n] = iteration
+      until not iteration
+   end)
+end
+
+function cofun.pairwise(iterator) return cofun.nwise(2, iterator) end
 
 function cofun.collect(iterator)
    local collection = {}
    for iteration in iterator do collection[#collection + 1] = iteration end
    return collection
+end
+
+function cofun.collect_n(n, iterator)
+   return cofun.collect(cofun.take(n, iterator))
 end
 
 function cofun.collectmap(iterator, key_fun, val_fun)
@@ -105,12 +198,16 @@ function cofun.collectmap(iterator, key_fun, val_fun)
    return map
 end
 
-function cofun.reduce(bi_fun, iterator, init_val)
-   local reduced = init_val or iterator()
-   if reduced then
-      for iteration in iterator do reduced = bi_fun(reduced, iteration) end
+function cofun.fold(bi_fun, init_val, iterator)
+   local folded = init_val
+   if folded then
+      for iteration in iterator do folded = bi_fun(folded, iteration) end
    end
-   return reduced
+   return folded
+end
+
+function cofun.foldfirst(bi_fun, iterator)
+   return cofun.fold(bi_fun, iterator(), iterator)
 end
 
 function cofun.any(iterator)
@@ -123,8 +220,146 @@ function cofun.all(iterator)
    return true
 end
 
+function cofun.len(iterator)
+   local counter = 0
+   for _ in iterator do counter = counter + 1 end
+   return counter
+end
+
+function cofun.interleave(iterator_1, iterator_2)
+   return coroutine.wrap(function()
+      local iteration_1 = iterator_1()
+      local iteration_2 = iterator_2()
+      while iteration_1 and iteration_2 do
+         coroutine.yield(iteration_1)
+         coroutine.yield(iteration_2)
+         iteration_1 = iterator_1()
+         iteration_2 = iterator_2()
+      end
+      while iteration_1 do
+         coroutine.yield(iteration_1)
+         iteration_1 = iterator_1()
+      end
+      while iteration_2 do
+         coroutine.yield(iteration_2)
+         iteration_2 = iterator_2()
+      end
+   end)
+end
+
+function cofun.intersperse(element, iterator)
+   return coroutine.wrap(function()
+      local iteration = iterator()
+      if not iteration then return end
+      coroutine.yield(iteration)
+      iteration = iterator()
+      while iteration do
+         coroutine.yield(element)
+         coroutine.yield(iteration)
+         iteration = iterator()
+      end
+   end)
+end
+
+function cofun.chunks(length, iterator)
+   return coroutine.wrap(function()
+      local chunk = {}
+      local counter = 1
+      local iteration = iterator()
+      while iteration do
+         if counter <= length then
+            chunk[counter] = iteration
+            counter = counter + 1
+            iteration = iterator()
+         else
+            coroutine.yield(chunk)
+            counter = 1
+         end
+      end
+      if counter > 1 then
+         for i = counter, length do chunk[i] = nil end
+         coroutine.yield(chunk)
+      end
+   end)
+end
+
+function cofun.find(predicate, iterator)
+   return cofun.first(cofun.filter(predicate, iterator))
+end
+
+function cofun.locate(element, iterator)
+   for i, iteration in cofun.enumerate(iterator) do
+      if iteration == element then return i end
+   end
+end
+
+function cofun.locateby(locator, iterator)
+   for i, iteration in cofun.enumerate(iterator) do
+      if locator(iteration) then return i end
+   end
+end
+
+function cofun.allequal(iterator)
+   local previous = iterator()
+   if not previous then return true end
+   for iteration in iterator do
+      if previous ~= iteration then return false end
+      previous = iteration
+   end
+   return true
+end
+
 function cofun.sum(iterator)
-   return cofun.reduce(function(left, right) return left + right end, iterator, 0)
+   local sum_fun = function(left, right) return left + right end
+   return cofun.reduce(sum_fun, iterator, 0)
+end
+
+function cofun.max(iterator)
+   local max_fun = function(left, right)
+      if left > right then
+         return left
+      else
+         return right
+      end
+   end
+   return cofun.reduce(max_fun, iterator)
+end
+
+function cofun.min(iterator)
+   local max_fun = function(left, right)
+      if left < right then
+         return left
+      else
+         return right
+      end
+   end
+   return cofun.reduce(max_fun, iterator)
+end
+
+function cofun.join(separator, iterator)
+   assert(type(separator) == 'string', 'Separator must be a string')
+   return table.concat(cofun.collect(cofun.intersperse(separator, iterator)))
+end
+
+function cofun.first(iterator) return iterator() end
+
+function cofun.nth(n, iterator) return cofun.first(cofun.skip(n - 1, iterator)) end
+
+function cofun.once(iterator) return cofun.take(1, iterator) end
+
+function cofun.oncewith(mapper, iterator)
+   return cofun.map(mapper, cofun.once(iterator))
+end
+
+function cofun.last(iterator)
+   local previous = iterator()
+   if not previous then return end
+   local iteration = iterator()
+   while iteration do
+      previous = iteration
+      iteration = iterator()
+   end
+   return previous
 end
 
 function cofun.negated(func) return function(...) return not func(...) end end
